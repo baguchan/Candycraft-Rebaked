3238a48bad09e7afd0a9018ba7d492ba082840c9
Sarinsa <mikkemusbruker@gmail.com>
2021-12-12 22:18:14 +0100

    Configured features are now registered with the mod's namespace included in their key name to avoid possible conflicts.

--- src/main/java/com/evo/candycraft/common/world/features/ConfiguredFeatures.java
+++ src/main/java/com/evo/candycraft/common/world/features/ConfiguredFeatures.java
@@ -1,5 +1,6 @@
 package com.evo.candycraft.common.world.features;
 
+import com.evo.candycraft.common.core.CandyCraft;
 import com.evo.candycraft.common.core.registry.CandyCraftFeatures;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.registry.Registry;
@@ -14,10 +15,13 @@ public class ConfiguredFeatures {
     public static ConfiguredFeature<BaseTreeFeatureConfig, ?> CANDY_CANE_TREE_FEATURE;
 
     public static void register() {
-        CANDY_CANE_TREE_FEATURE = register("candy_cane_tree", CandyCraftFeatures.CANDY_CANE_TREE.get().withConfiguration(new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.AIR.getDefaultState()), new SimpleBlockStateProvider(Blocks.AIR.getDefaultState()), new BlobFoliagePlacer(FeatureSpread.func_242252_a(0), FeatureSpread.func_242252_a(0), 0), new StraightTrunkPlacer(0, 0, 0), new TwoLayerFeature(0, 0, 0)).setIgnoreVines().build()));
+        CANDY_CANE_TREE_FEATURE = register("candy_cane_tree", CandyCraftFeatures.CANDY_CANE_TREE.get()
+                .withConfiguration(new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.AIR.getDefaultState()), new SimpleBlockStateProvider(Blocks.AIR.getDefaultState()), new BlobFoliagePlacer(FeatureSpread.func_242252_a(0), FeatureSpread.func_242252_a(0), 0), new StraightTrunkPlacer(0, 0, 0), new TwoLayerFeature(0, 0, 0))
+                        .setIgnoreVines()
+                        .build()));
     }
 
     private static <C extends IFeatureConfig> ConfiguredFeature<C, ?> register(String key, ConfiguredFeature<C, ?> configuredFeature) {
-        return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, key, configuredFeature);
+        return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, CandyCraft.MODID + ":" + key, configuredFeature);
     }
 }
